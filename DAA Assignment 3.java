8f69c7ab96e92966f4993c90b3866bb641b4bffb
Ishan Dixit <113436813+Ishan0704@users.noreply.github.com>
2024-11-06 08:17:07 +0530

    Update DAA Assignment 3.java

--- DAA Assignment 3.java
+++ DAA Assignment 3.java
@@ -16,38 +16,38 @@ public class Assignment3 {
        }
     }
 
-    public static double solution(int capacity, ArrayList<Item> item)
+    public static double solution(int capacity, Item [] item)
     {
         Sort(item);
 
         double maxProfit = 0;
 
-        for(Item i:item)
-        {
-            if(capacity >= i.weight)
+        for(int i=0;i<item.length;i++)
+        { 
+            if(capacity >= item[i].weight)
             {
-                capacity = capacity - i.weight;
-                maxProfit += i.profit;
+                capacity = capacity - item[i].weight;
+                maxProfit += item[i].profit;
             }
             else{
-                maxProfit += i.profit * ((double)(capacity/i.weight));
+                maxProfit += item[i].profit * ((double)(capacity/item[i].weight));
                 break;
             }
         }
         return maxProfit;
     }
 
-    public static void Sort(ArrayList<Item> item)
+    public static void Sort(Item []item)
     {
-        for(int i=0 ; i<item.size()-1; i++)
+        for(int i=0 ; i<item.length-1; i++)
         {
-            for(int j=0 ;j<item.size()-i-1;j++)
+            for(int j=0 ;j<item.length-i-1;j++)
             {
-                if(item.get(j).ratio < item.get(j+1).ratio)
+                if(item[j].ratio < item[j+1].ratio)
                 {
-                    Item temp = item.get(j);
-                    item.set(j , item.get(j+1));
-                    item.set(j+1,temp);
+                    Item temp = item[j];
+                    item[j] = item[j+1];
+                    item[j+1] = temp;
                 }
             }
         }
@@ -60,16 +60,18 @@ public class Assignment3 {
         int profit []={5,10,15,7,8,9,4};
         int weight [] ={1,3,5,4,1,3,2};
 
-        ArrayList <Item> list = new ArrayList<>();
+        Item items[]= new Item[profit.length];
+        // ArrayList <Item> list = new ArrayList<>();
 
         for(int i=0;i<profit.length ;i++)
         {
-            list.add( new Item ( weight[i],profit[i]));
+            // list.add( new Item ( weight[i],profit[i]));
+            items[i] = new Item(weight[i],profit[i]);
         }
 
         // Item item[] = {new Item(1,5),new Item(3,10),new Item(5,15),new Item(4,7), new Item(1,8),new Item(3,9) ,new Item(2,4)};
 
-        double ans = solution(bag_capacity,list);
+        double ans = solution(bag_capacity,items);
         System.out.println("Total value : "+ans);
     }
 }
